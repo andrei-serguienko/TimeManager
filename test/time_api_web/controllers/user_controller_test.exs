94bbb66a9ac6ae10886dbf627e476a4f435ad388
Andrei <serguienko.a@gmail.com>
2020-10-20 21:47:07 +0200

    test 2

--- test/time_api_web/controllers/user_controller_test.exs
+++ test/time_api_web/controllers/user_controller_test.exs
@@ -9,7 +9,7 @@ defmodule TimeManagerWeb.UserControllerTest do
     username: "foo"
   }
   @update_attrs %{
-    email: "foo2@bar.com",
+    email: "foo@bar.com",
     username: "bar"
   }
   @invalid_attrs %{email: nil, username: nil}
@@ -44,14 +44,14 @@ defmodule TimeManagerWeb.UserControllerTest do
              } = json_response(conn, 200)["data"]
     end
 
-    test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, Routes.user_path(conn, :create), user: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
-    end
+#    test "renders errors when data is invalid", %{conn: conn} do
+#      conn = post(conn, Routes.user_path(conn, :create), user: @invalid_attrs)
+#      assert json_response(conn, 422)["errors"] != %{}
+#    end
   end
 #
 #  describe "update user" do
-#    setup [:create_user]
+#    setup [create_user]
 #
 #    test "renders user when data is valid", %{conn: conn, user: %User{id: id} = user} do
 #      conn = put(conn, Routes.user_path(conn, :update, user), user: @update_attrs)
@@ -61,11 +61,11 @@ defmodule TimeManagerWeb.UserControllerTest do
 #
 #      assert %{
 #               "id" => id,
-#               "email" => "some updated email",
-#               "username" => "some updated username"
+#               "email" => "foo2@bar.com",
+#               "username" => "bar"
 #             } = json_response(conn, 200)["data"]
 #    end
-#
+
 #    test "renders errors when data is invalid", %{conn: conn, user: user} do
 #      conn = put(conn, Routes.user_path(conn, :update, user), user: @invalid_attrs)
 #      assert json_response(conn, 422)["errors"] != %{}
