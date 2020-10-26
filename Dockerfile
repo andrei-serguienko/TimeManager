e72e8e2a35f27293f1e1598ed5708cb23831e611
Andrei <serguienko.a@gmail.com>
2020-10-26 11:15:17 +0100

    Config heroku

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM elixir:1.9
+# install hex package manager
+RUN mix local.hex --force
+# install phoenix
+RUN mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez --force
+
+RUN mkdir /api
+COPY . /api
+WORKDIR /api
+
+ENV MIX_ENV=prod
+ENV PORT=4000
+ENV DATABASE_URL=ecto://postgres:postgres@db/time_api_dev
+ENV SECRET_KEY_BASE=t8sxAm3YJK0ekRZO9vIfIMrm8vjz9OdEjs9v4pgp2wlz5sRbU8ZYcV9CqwSUyBqy
+
+RUN mix local.rebar --force
+RUN mix deps.get --only prod
+RUN mix compile
+RUN mix phx.digest
+# run phoenix in production on PORT 4000
+CMD mix ecto.setup && mix phx.server
--- config/prod.exs
+++ config/prod.exs
@@ -11,16 +11,13 @@ use Mix.Config
 # before starting your production server.
 config :time_api,
        TimeManagerWeb.Endpoint,
+       http: [port: {:system, "PORT"}],
        url: [
          scheme: "https",
-         host: "git.heroku.com/mysterious-wildwood-59631.git",
+         host: "evening-atoll-20623.herokuapp.com/",
          port: 443
        ],
-       force_ssl: [
-         rewrite_on: [:x_forwarded_proto]
-       ],
-       cache_static_manifest: "priv/static/cache_manifest.json",
-       secret_key_base: System.get_env("SECRET_KEY_BASE")
+       force_ssl: [rewrite_on: [:x_forwarded_proto]],
 
 # Do not print debug messages in production
 config :logger, level: :info
