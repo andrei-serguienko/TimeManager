12bb4103c0ef07c63b28c3b71d22f96a4e97bd7a
Andrei <serguienko.a@gmail.com>
2020-10-26 14:22:33 +0100

    Changes

--- mix.exs
+++ mix.exs
@@ -49,6 +49,7 @@ defmodule TimeManager.MixProject do
   # Aliases are shortcuts or tasks specific to the current project.
   # For example, to install project dependencies and perform other setup tasks, run:
   #
+  #
   #     $ mix setup
   #
   # See the documentation for `Mix` for more info on aliases.
